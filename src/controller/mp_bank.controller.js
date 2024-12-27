19c77a6060d35e6d2fd6e810f9600bc0be9bce83
theoxgn <aaluff112@gmail.com>
2024-12-27 09:54:36 +0700

    fix

--- src/controller/mp_bank.controller.js
+++ src/controller/mp_bank.controller.js
@@ -24,64 +24,6 @@ class MPBankController {
             next(error)
         }
     }
-
-    async getBankAccountById(req, res, next) {
-        try {
-            let validRequest = validationResult(req);
-            if (!validRequest.isEmpty()) {
-                throw new ResponseError(400, validRequest.array(), 'Validation failed');
-            }
-    
-            const userID = 99
-            const result = await MPBankService.getBankAccountById(req.params.id, userID);
-    
-            return await MessageHelper.showMessage(200, {
-                Data: result,
-                Type: req.originalUrl,
-            }, true, res);
-        } catch (error) {
-            next(error)
-        }
-    }
-
-    async editBankAccount(req, res, next) {
-        try {
-            let validRequest = validationResult(req);
-            if (!validRequest.isEmpty()) {
-                throw new ResponseError(400, validRequest.array(), 'Validation failed');
-            }
-    
-            const userID = 99
-            const result = await MPBankService.editBankAccount(req.params.id, req.body, userID);
-    
-            return await MessageHelper.showMessage(200, {
-                Data: result,
-                Type: req.originalUrl,
-            }, true, res);
-        } catch (error) {
-            next(error)
-        }
-    }
-
-    async deleteBankAccount(req, res, next) {
-        try {
-            let validRequest = validationResult(req);
-            if (!validRequest.isEmpty()) {
-                throw new ResponseError(400, validRequest.array(), 'Validation failed');
-            }
-    
-            const userID = 99
-            const result = await MPBankService.deleteBankAccount(req.params.id, userID);
-    
-            return await MessageHelper.showMessage(200, {
-                Data: result,
-                Type: req.originalUrl,
-            }, true, res);
-        } catch (error) {
-            next(error)
-        }
-    }
-
     
 }
 
--- src/routes/v1/muatparts/bank.js
+++ src/routes/v1/muatparts/bank.js
@@ -5,9 +5,6 @@ const MPBankController = require('../../../controller/mp_bank.controller');
 
 // CRUD bank account
 router.get("/bankAccount", MPBankController.getAllBankAccount)
-router.get("/bankAccount/:id", MPBankController.getBankAccountById)
-router.put("/bankAccount/:id", MPBankController.editBankAccount)
-router.delete("/bankAccount/:id", MPBankController.deleteBankAccount)
 
 
 module.exports = router;
